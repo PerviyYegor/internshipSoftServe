fce8beb48a53268660606d80c770d273a08164ad
PerviyYegor <yegor.perviy@gmail.com>
2023-11-16 21:51:11 +0200

    monitoring task complete

--- task4/vagrantVM/nagiosMonitoring.sh
+++ task4/vagrantVM/nagiosMonitoring.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 sudo yum install -y nrpe python3 wget
 sudo yum install -y nagios-plugins-{load,http,users,procs,disk,swap,nrpe,uptime} -y
-wget -O /usr/local/bin/check_docker https://raw.githubusercontent.com/timdaman/check_docker/master/check_docker/check_docker.py
+sudo mv /home/vagrant/check_docker /usr/local/bin/check_docker
 chmod +x /usr/local/bin/check_docker
+sudo usermod -aG docker nrpe
+
 
 
 sudo mv /home/vagrant/nrpe.cfg /etc/nagios/nrpe.cfg 
 sudo systemctl enable nrpe
 sudo systemctl restart nrpe
+sudo systemctl restart docker
+docker start apache
---
+++ task5/nagiosInstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo yum -y install epel-release
+sudo yum install nano  nagios nagios-plugins-{load,http,users,procs,disk,swap,nrpe,uptime} -y
+sudo yum install -y nrpe
+sudo systemctl start nagios
+sudo systemctl enable nagios
+sudo htpasswd -c /etc/nagios/passwd nagiosadmin
+sudo setenforce 0
+
+sudo sh -c " echo 'cfg_file=/etc/nagios/servers/remote-server.cfg' >>/etc/nagios/nagios.cfg"
+
+sudo mkdir /etc/nagios/servers
+
+sudo mv /home/vagrant/remote-server.cfg  /etc/nagios/servers/remote-server.cfg
+sudo chmod 755 /etc/nagios/servers/remote-server.cfg
+sudo chown root:root /etc/nagios/servers/remote-server.cfg
+
+sudo sed -i 's/Require ip 127.0.0.1/#Require ip 127.0.0.1/' /etc/httpd/conf.d/nagios.conf
+sudo systemctl restart httpd
+
+sudo sh -c "echo "allowed_hosts=127.0.0.1,192.168.5.10,192.168.7.10,192.168.10.10">>/etc/nagios/nrpe.cfg"
+sudo systemctl enable nrpe
+sudo systemctl start nrpe
+
+sudo nagios -v /etc/nagios/nagios.cfg
+sudo systemctl restart nagios
