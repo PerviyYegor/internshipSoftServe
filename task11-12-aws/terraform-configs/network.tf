8b7a401443008a2516a1954401369e677f60b638
PerviyYegor <yegor.perviy@gmail.com>
2023-12-06 13:17:27 +0200

    clean cached dirs

--- task11-12-aws/terraform-configs/network.tf
+++
@@ -1,8 +0,0 @@
-data "aws_vpc" "default" {
-  default = true
-} 
-
-resource "aws_default_subnet" "main" {
-  availability_zone = "${var.aws_region}a"
-}
-
--- task11-12-aws/terraform-configs/output.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip" { 
-  value = aws_instance.prometheus.public_ip 
-  description = "The public IP of the prometheus server" 
-} 
