2b88566ae21d411b3e7c00d7d043091030f63050
PerviyYegor <yegor.perviy@gmail.com>
2023-12-25 20:08:27 +0200

    separated loki and grafana to another ec2 and made connect from goapp

---
+++ task17-aws/terraform-configs/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region"{
+    type = string
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16" 
+}
+
+variable "subnet_cidr" {
+    type = string
+    default = "10.0.1.0/24"
+}
+
+variable "pathToSaveSshKey" {
+    type = string
+    default = "../ssh-key.pem"
+    description = "Path where terraform should save generated SSH private key"
+}
+
+variable "pairKeyName" {
+    type = string
+    default = "loki-grafanaAppTerraform"
+}
+
+variable "instance_tag" {
+    type = string
+    default = "loki-grafana"
+    description = "tag to identificate instance"
+}
+
+variable "allowed_ports" {
+  description = "List of allowed ports"
+  type        = list(number)
+  default     = [22, 3000, 3100]
+}
